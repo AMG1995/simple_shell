9a988a803ae6204488606a9724af5553993f316c
AMG1995 <39673621+AMG1995@users.noreply.github.com>
2023-10-15 20:43:18 +0400

    Update environ.c

--- 0x01_Simple/environ.c
+++ 0x01_Simple/environ.c
@@ -81,9 +81,9 @@ int populate_env_list(info_t *info)
 	size_t i;
 	i = 0;
 
-	while (enviro_array[i]) 
+	while (environ[i]) 
 		{
-    		add_node_end(&n, enviro_array[i], 0);
+    		add_node_end(&n, environ[i], 0);
 		i++;
 		}
 	info->env = n;
