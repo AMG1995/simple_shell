50cfd418f4b3372a1b2996828c8eb9cdb05110fb
AMG1995 <39673621+AMG1995@users.noreply.github.com>
2023-10-17 19:30:42 +0400

    Update builtin1.c

--- 0x01_Simple/builtin1.c
+++ 0x01_Simple/builtin1.c
@@ -1,115 +1,112 @@
 #include "shell.h"
 
 /**
- * _myhistory - displays the history list, one command by line, preceded
- *              with line numbers, starting at 0.
+ * display_history - Displays the history list, one command per line, preceded
+ *                  with line numbers, starting at 0.
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
- *  Return: Always 0
+ * Return: Always 0
  */
-int _myhistory(info_t *info)
+int display_history(info_t *info)
 {
-	print_linked_list(info->history);
-	return (0);
+    print_linked_list(info->history);
+    return (0);
 }
 
 /**
- * unset_alias - sets alias to string
- * @info: parameter struct
- * @str: the string alias
- *
- * Return: Always 0 on success, 1 on error
+ * unset_alias_variable - Unsets an alias variable.
+ * @info: Parameter struct.
+ * @str: The alias variable to unset.
+ * Return: Always 0 on success, 1 on error.
  */
-int unset_alias(info_t *info, char *str)
+int unset_alias_variable(info_t *info, char *str)
 {
-	char *p, c;
-	int ret;
+    char *equal_sign, saved_char;
+    int ret;
 
-	p = _strchr(str, '=');
-	if (!p)
-		return (1);
-	c = *p;
-	*p = 0;
-	ret = delete_point_index(&(info->alias),
-		get_index_of_node(info->alias, find_node_with_prefix(info->alias, str, -1)));
-	*p = c;
-	return (ret);
+    equal_sign = _strchr(str, '=');
+    if (!equal_sign)
+        return (1);
+    saved_char = *equal_sign;
+    *equal_sign = 0;
+    ret = delete_point_index(&(info->alias),
+                            get_node_index(info->alias, find_node_with_prefix(info->alias, str, -1)));
+    *equal_sign = saved_char;
+    return (ret);
 }
 
 /**
- * set_alias - sets alias to string
- * @info: parameter struct
- * @str: the string alias
- *
- * Return: Always 0 on success, 1 on error
+ * set_alias_variable - Sets an alias variable.
+ * @info: Parameter struct.
+ * @str: The alias variable to set.
+ * Return: Always 0 on success, 1 on error.
  */
-int set_alias(info_t *info, char *str)
+int set_alias_variable(info_t *info, char *str)
 {
-	char *p;
+    char *equal_sign;
 
-	p = _strchr(str, '=');
-	if (!p)
-		return (1);
-	if (!*++p)
-		return (unset_alias(info, str));
+    equal_sign = _strchr(str, '=');
+    if (!equal_sign)
+        return (1);
+    if (!*++equal_sign)
+        return (unset_alias_variable(info, str));
 
-	unset_alias(info, str);
-	return (add_point_end(&(info->alias), str, 0) == NULL);
+    unset_alias_variable(info, str);
+    return (add_point_end(&(info->alias), str, 0) == NULL);
 }
 
 /**
- * print_alias - prints an alias string
- * @node: the alias node
- *
- * Return: Always 0 on success, 1 on error
+ * print_alias_variable - Prints an alias variable.
+ * @node: The alias variable node.
+ * Return: Always 0 on success, 1 on error.
  */
-int print_alias(list_t *node)
+int print_alias_variable(list_t *node)
 {
-	char *p = NULL, *a = NULL;
+    char *equal_sign, *alias_str;
 
-	if (node)
-	{
-		p = _strchr(node->str, '=');
-		for (a = node->str; a <= p; a++)
-			_putchar(*a);
-		_putchar('\'');
-		_puts(p + 1);
-		_puts("'\n");
-		return (0);
-	}
-	return (1);
+    if (node)
+    {
+        equal_sign = _strchr(node->str, '=');
+        for (alias_str = node->str; alias_str <= equal_sign; alias_str++)
+            _putchar(*alias_str);
+        _putchar('\'');
+        _puts(equal_sign + 1);
+        _puts("'\n");
+        return (0);
+    }
+    return (1);
 }
 
 /**
- * _myalias - mimics the alias builtin (man alias)
+ * manage_alias - Mimics the alias builtin (man alias).
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
- *  Return: Always 0
+ * Return: Always 0
  */
-int _myalias(info_t *info)
+int manage_alias(info_t *info)
 {
-	int i = 0;
-	char *p = NULL;
-	list_t *node = NULL;
+    int i;
+    char *equal_sign;
+    list_t *node;
 
-	if (info->argc == 1)
-	{
-		node = info->alias;
-		while (node)
-		{
-			print_alias(node);
-			node = node->next;
-		}
-		return (0);
-	}
-	for (i = 1; info->argv[i]; i++)
-	{
-		p = _strchr(info->argv[i], '=');
-		if (p)
-			set_alias(info, info->argv[i]);
-		else
-			print_alias(find_node_with_prefix(info->alias, info->argv[i], '='));
-	}
+    if (info->argc == 1)
+    {
+        node = info->alias;
+        while (node)
+        {
+            print_alias_variable(node);
+            node = node->next;
+        }
+        return (0);
+    }
+    for (i = 1; info->argv[i]; i++)
+    {
+        equal_sign = _strchr(info->argv[i], '=');
+        if (equal_sign)
+            set_alias_variable(info, info->argv[i]);
+        else
+            print_alias_variable(find_node_with_prefix(info->alias, info->argv[i], '='));
+    }
 
-	return (0);
+    return (0);
 }
