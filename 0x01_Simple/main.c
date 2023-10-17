0694b3b648758cc8dd7052d3512f58602188d554
AMG1995 <39673621+AMG1995@users.noreply.github.com>
2023-10-17 22:35:20 +0400

    Update main.c

--- 0x01_Simple/main.c
+++ 0x01_Simple/main.c
@@ -26,11 +26,11 @@ int main(int ac, char **av)
 				exit(126);
 			if (errno == ENOENT)
 			{
-				_eputs(av[0]);
-				_eputs(": 0: Can't open ");
+				write_stderr(av[0]);
+				write_stderr(": 0: Can't open ");
 				_eputs(av[1]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
+				write_stderr_char('\n');
+				write_stderr_char(BUF_FLUSH);
 				exit(127);
 			}
 			return (EXIT_FAILURE);
