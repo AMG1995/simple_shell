21b343805b2d5efe2d98f1e29709ea5b42aaa5e0
AMG1995 <39673621+AMG1995@users.noreply.github.com>
2023-10-18 14:36:26 +0400

    Update main.c

--- 0x01_Simple/main.c
+++ 0x01_Simple/main.c
@@ -2,43 +2,49 @@
 
 /**
  * main - entry point
- * @ac: arg count
- * @av: arg vector
+ * @ac: argument count
+ * @av: argument vector
  *
  * Return: 0 on success, 1 on error
  */
 int main(int ac, char **av)
 {
-	info_t info[] = { INFO_INIT };
-	int fd = 2;
+    info_t info;
+    memset(&info, 0, sizeof(info_t));
 
-	asm ("mov %1, %0\n\t"
-		"add $3, %0"
-		: "=r" (fd)
-		: "r" (fd));
+    int fd = 2;
+    __asm__(
+        "mov %1, %0\n\t"
+        "add $3, %0"
+        : "=r"(fd)
+        : "r"(fd));
 
-	if (ac == 2)
-	{
-		fd = open(av[1], O_RDONLY);
-		if (fd == -1)
-		{
-			if (errno == EACCES)
-				exit(126);
-			if (errno == ENOENT)
-			{
-				write_stderr(av[0]);
-				write_stderr(": 0: Can't open ");
-				write_stderr(av[1]);
-				write_stderr_char('\n');
-				write_stderr_char(BUF_FLUSH);
-				exit(127);
-			}
-			return (EXIT_FAILURE);
-		}
-		info->readfd = fd;
-	}
-	populate_env_list(info);
-	read_history(info);
-	hsh(info, av);
-	return (EXIT_SUCCESS);
+    if (ac == 2)
+    {
+        fd = open(av[1], O_RDONLY);
+        if (fd == -1)
+        {
+            if (errno == EACCES)
+            {
+                exit(126);
+            }
+            if (errno == ENOENT)
+            {
+                write_stderr(av[0]);
+                write_stderr(": 0: Can't open ");
+                write_stderr(av[1]);
+                write_stderr(": No such file or directory\n");
+                write_stderr_char(BUF_FLUSH);
+                exit(127);
+            }
+            return (EXIT_FAILURE);
+        }
+        info.readfd = fd;
+    }
+
+    populate_env_list(&info);
+    read_history(&info);
+    hsh(&info, av);
+
+    return (EXIT_SUCCESS);
 }
