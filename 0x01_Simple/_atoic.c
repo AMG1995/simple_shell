e01ef1344bd039e8fec93f4176b4bf0d696e0adf
AMG1995 <39673621+AMG1995@users.noreply.github.com>
2023-10-18 17:43:00 +0400

    Update _atoic.c

--- 0x01_Simple/_atoic.c
+++ 0x01_Simple/_atoic.c
@@ -1,57 +1,56 @@
 #include "shell.h"
 
 /**
- * is_interactive - Returns true if the shell is in interactive mode
- * @info: Address of the struct
+ * interactive - returns true if shell is interactive mode
+ * @info: struct address
  *
- * Return: 1 if in interactive mode, 0 otherwise
+ * Return: 1 if interactive mode, 0 otherwise
  */
-int is_interactive(info_t *info)
+int interactive(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
- * is_delimiter - Checks if a character is a delimiter
- * @c: The character to check
- * @delim: The delimiter string
+ * is_delim - checks if character is a delimeter
+ * @c: the char to check
+ * @delim: the delimeter string
  * Return: 1 if true, 0 if false
  */
-int is_delimiter(char c, char *delim)
+int is_delim(char c, char *delim)
 {
 	while (*delim)
-	{
-		if (*delim == c)
-			return 1;
-		delim++;
-	}
-	return 0;
+		if (*delim++ == c)
+			return (1);
+	return (0);
 }
 
 /**
- * is_alpha - Checks for an alphabetic character
- * @c: The character to check
- * Return: 1 if c is alphabetic, 0 otherwise
+ *_isalpha - checks for alphabetic character
+ *@c: The character to input
+ *Return: 1 if c is alphabetic, 0 otherwise
  */
-int is_alpha(int c)
+
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return 1;
+		return (1);
 	else
-		return 0;
+		return (0);
 }
 
 /**
- * string_to_int - Converts a string to an integer
- * @s: The string to be converted
- * Return: 0 if no numbers in the string, the converted number otherwise
+ *_atoi - converts a string to an integer
+ *@s: the string to be converted
+ *Return: 0 if no numbers in string, converted number otherwise
  */
-int string_to_int(char *s)
+
+int _atoi(char *s)
 {
 	int i, sign = 1, flag = 0, output;
 	unsigned int result = 0;
 
-	for (i = 0; s[i] != '\0' && flag != 2; i++)
+	for (i = 0;  s[i] != '\0' && flag != 2; i++)
 	{
 		if (s[i] == '-')
 			sign *= -1;
@@ -71,5 +70,6 @@ int string_to_int(char *s)
 	else
 		output = result;
 
-	return output;
+	return (output);
 }
+
